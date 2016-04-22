9f4aa8ecd5bbc9d9b26c3a2babb49c621531c088
Bradley Meyers <meyers.bradley@hotmail.com>
2016-04-22 09:38:01 +0800

    Refined selection tool.

--- interactive/interactive_sp_plot.py
+++ interactive/interactive_sp_plot.py
@@ -193,36 +193,23 @@ def singlepulse_plot(basename=None, DMvTime=1, StatPlots=False, raw=False, thres
     # ensure only using data points with sigma > threshold
     data = data[data['Sigma']>threshold]
 
-
-    #DM = [float(row.split()[0]) for row in data if float(row.split()[1]) >= threshold]
-    #Sigma = [float(row.split()[1]) for row in data if float(row.split()[1]) >= threshold]
-    #Time = [float(row.split()[2]) for row in data if float(row.split()[1]) >= threshold]
-    #Sample = [int(row.split()[3]) for row in data if float(row.split()[1]) >= threshold]
-    #Downfact = [int(row.split()[4]) for row in data if float(row.split()[1]) >= threshold]
-    
-    #DM = data.dm_list
-    #Sigma = data.sigma_list
-    #Time = data.time_list
-    #Downfact = data.downfact_list
-
-    #Downfact_float = data.downfact_list.astype(float)
-
-    fig = plt.figure()
     cm = plt.cm.get_cmap('gist_rainbow')
-
 
     Size = 5 * data['Sigma'].values/4.0
     percentile = np.percentile(Size, 99.5)
     Size[np.where(Size > 5 * percentile)] = 5 * percentile/4.0
 
     TOOLS = "resize,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
-    color= [navy]*len(Size)
+    color = 'navy'
+    color_array= [color]*len(Size)
     source = ColumnDataSource(data=dict(time=data['Time'].values,\
                                         dm=data['DM'].values,\
                                         sigma=data['Sigma'].values,\
-                                        size=Size,color=color,\
+                                        size=Size,color=color_array,\
                                         downfact=data['Downfact'].values.astype(float)))
     
+
+    source2 = ColumnDataSource(data=dict(time=[],dm=[],sigma=[]))
 
     timeseries = figure(plot_width=900, plot_height=400, webgl=True,tools=TOOLS)
     timeseries.scatter('time','dm',source=source, marker='o',size='size',color='color')
@@ -230,25 +217,27 @@ def singlepulse_plot(basename=None, DMvTime=1, StatPlots=False, raw=False, thres
     timeseries.yaxis.axis_label = 'DM'
 
     if StatPlots:
-        top_left = figure(plot_width=300, plot_height=300 ,webgl=True,tools=TOOLS)
+        top_left = figure(plot_width=300, plot_height=300 ,webgl=True, tools=TOOLS)
         hist,edges = np.histogram(data['Sigma'].values, bins=50)
         #source.add(hist,name='sigma_hist')
-        top_left.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],color='color')
+        top_left.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],line_color=color,fill_color=color)
         top_left.xaxis.axis_label = 'S/N'
         top_left.yaxis.axis_label = 'Number of pulses'
 
         top_mid = figure(plot_width=300, plot_height=300, webgl=True,tools=TOOLS)
         hist,edges = np.histogram(data['DM'].values, bins=50)
         #source.add(hist,name='dm_hist')
-        top_mid.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],color='color')
+        top_mid.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],line_color=color,fill_color=color)
         top_mid.xaxis.axis_label = 'DM'
         top_mid.yaxis.axis_label = 'Number of pulses'
 
-        top_right = figure(plot_width=300, plot_height=300, webgl=True,tools=TOOLS)
+        top_right = figure(plot_width=300, plot_height=300, webgl=True,tools=TOOLS,\
+                           x_range=top_mid.x_range)
         top_right.scatter('dm','sigma',source=source, marker='o',color='color')
         top_right.xaxis.axis_label = 'DM'
         top_right.yaxis.axis_label = 'S/N'
 
+        
 
     if StatPlots:
         plots = [gridplot([[top_left,top_mid,top_right]]),timeseries]
@@ -257,129 +246,6 @@ def singlepulse_plot(basename=None, DMvTime=1, StatPlots=False, raw=False, thres
         show(timeseries)
 
 
-        
-
-
-
-
-"""        
-    if StatPlots:
-        ax0 = fig.add_subplot(231)
-        plt.hist(data['Sigma'], histtype='step', bins=int(0.2 * len(set(data['Sigma'].values))))
-        ax0.set_xlabel('Signal-to-Noise', fontsize=18)
-        ax0.set_ylabel('Number of Pulses', fontsize=18)
-        #ax0.set_xlim([data['Sigma'].min(), data['Sigma'].max()])
-
-        ax1 = fig.add_subplot(232)
-        plt.hist(data['DM'], histtype='step', bins=int(0.5 * len(set(data['DM'].values))))
-        ax1.set_xlabel('DM ($\mathrm{pc\, cm^{-3}}$)', fontsize=18)
-        ax1.set_ylabel('Number of Pulses', fontsize=18)
-        #ax1.set_xlim([data.dm_list.min(), data.dm_list.max()])
-
-        ax2 = fig.add_subplot(233, sharex=ax1) 
-        # BWM: now shares x-axis with ax1, so changing DM on one will change range on the other
-        plt.scatter(data['DM'], data['Sigma'], c=data['Downfact'].astype(float), cmap=cm, alpha=0.9)
-        ax2.set_ylabel('Signal-to-Noise', fontsize=18)
-        ax2.set_xlabel('DM ($\mathrm{p\, cm^{-3}}$)', fontsize=18)
-        #ax2.set_xlim([data.dm_list.min(), data.dm_list.max()])
-        #ax2.set_ylim([data.sigma_list.min(), data.sigma_list.max()])
-
-        ax3 = fig.add_subplot(212)
-
-    else:
-        ax3 = fig.add_subplot(111)
-
-    # TODO: need to figure out how (if at all) we can make the axis sharing work 
-    #       for x-axis to y-axis share
-
-#	ax3.set_title("Single Pulse Sigma")    
-    ax3.set_xlabel('Time (s)', fontsize=18)
-    ax3.set_ylabel('DM ($\mathrm{pc\, cm^{-3}}$)', fontsize=18)
-    #ax3.set_ylim([data.dm_list.min(), data.dm_list.max()])
-    #ax3.set_xlim([data.time_list.min(), data.time_list.max()])
-    #ax3.set_ylabel('Time (s)', fontsize=18)
-    #ax3.set_xlabel('DM ($\mathrm{pc\, cm^{-3}}$)', fontsize=18)
-    #ax3.set_ylim([data.time_list.min(), data.time_list.max()])
-    #ax3.set_xlim([data.dm_list.min(), data.dm_list.max()])
-    #cm = plt.cm.get_cmap('gist_rainbow')
-
-    # grab axis3 size to allocate marker sizes
-    #bbox_pix = ax3.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    #width, height = bbox_pix.width, bbox_pix.height
-    #area = width * height # axes area in inches^2 (apparently)
-
-    #TODO: need to try and use something like percentiles to make sure that just one 
-    # big pulse doesn't swamp the sizes or colorbars.
-    Size = 5 * data['Sigma'].values
-    percentile = np.percentile(Size, 99)
-    print "Allocating maximum marker size to be 1.2 * 99th percentile = {0}".format(5*percentile)   
-    Size[np.where(Size > 5 * percentile)] = 5 * percentile
-   
-    #Size = (3. * area / 2.) * (data['Sigma'].values**2 / percentile)
-    #Size[np.where(Size > percentile)] = (3. * area / 2.) * percentile
-
-
-
-    #Size=1
-    obs_stats(data['total_samples'].values[0] * data['dt'].values[0], flags)
-#    sc=ax3.scatter(Time,DM, s=Size, c=Sigma, vmin=min(Sigma), vmax=max(Sigma),\
-#                       cmap=cm, picker=1)
-    sc = ax3.scatter(data['Time'], data['DM'], s=Size, c=data['Downfact'].astype(float), cmap=cm, \
-                         vmin=data['Downfact'].astype(float).min(), vmax=data['Downfact'].astype(float).max(), facecolors='none')
-#    sc = ax3.scatter(data.dm_list, data.time_list, s=Size, c=Downfact_float, cmap=cm, \
-#                         vmin=Downfact_float.min(), vmax=Downfact_float.max(), picker=1, facecolor='none')
-#	leg = ax1.legend()
-         
-    #plt.colorbar(sc, label="Sigma", pad=0.01) 
-    #plt.colorbar(sc, label="Downfact", pad=0.01)
-    # BWM: can't seem to get the bottom plot to extend the entire width when the color bar is active.
-    fig.subplots_adjust(hspace=0.2, wspace=0.5)
-
-    if not raw:
-        if any(isinstance(l, np.ndarray) for l in flags):
-            for flag in flags:
-                if flag[0] != flag[1]:
-                    flag_area = patches.Rectangle((float(flag[0]), data['DM'].min()),\
-                                                  (float(flag[1]) - float(flag[0])),\
-                                                  (data['DM'].max() - data['DM'].min()),\
-                                                  edgecolor='0', facecolor='0.66')
-                    ax3.add_patch(flag_area)
-
-        else:
-            if flags[0] != flags[1]:
-                flag_area = patches.Rectangle((float(flags[0]), data['DM'].min()),\
-                                              (float(flags[1]) - float(flags[0])),\
-                                              (data['DM'].max() - data['DM'].min()),\
-                                              edgecolor='0', facecolor='0.66')
-                ax3.add_patch(flag_area)
-
-
-
-
-    def onpick(event):
-        points = event.artist
-        ind = event.ind
-        mouseevent = event.mouseevent
-        print '\n'
-        print "Information for data points around click event %.4f, %.4f:" % (mouseevent.xdata,  mouseevent.ydata)
-        for i in ind:		# These are fudge factors to turn samples into ms. 
-            if ( data.dm_list[i] < 150):
-                boxcar = data.downfact_list[i]
-            elif ( 150<= data.dm_list[i] < 823.2 ):
-                boxcar = data.downfact_list[i] * 2
-            elif ( 823.2 <= data.dm_list[i] < 1486.2):
-                boxcar = data.downfact_list[i] * 2
-            elif ( 1426.2 <= data.dm_list[i] < 2100):
-                boxcar = data.downfact_list[i] * 2
-            print "%.2f seconds, %.2f Sigma event detected at a DM of %.2f with a boxcar of: %d ms" % (data['Time'].values[i], data['Sigma'].values[i], data['DM'].values[i], boxcar)
-"""
-
-    #fig.canvas.mpl_connect('pick_event', onpick)
-
-    #fig.suptitle('Single Pulse Search results for ' + basename) 
-    #push_notebook()
-    #show(fig)
-    #plt.show()
 
             
 
